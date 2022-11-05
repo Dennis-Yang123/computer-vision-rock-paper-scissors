2c6b79978f4d90f2c8bf0535dadf1e5b95282151
Dennis Yang <dennisyang3.14@gmail.com>
2022-11-05 18:56:32 +0000

    Milestone 4 Task 1

--- manual_rps.py
+++ manual_rps.py
@@ -1,1 +1,15 @@
+# %%
+import random
+rps = ["rock", "paper", "scissors"]
+def get_computer_choice():
+    comp_choice = random.choice(rps)
+    print(comp_choice)
+
+def get_user_choice():
+    user_choice = input("Please choose between rock, paper or scissors")
+    user_choice = user_choice.lower()
+    if user_choice in rps:
+        print(user_choice)
+    else:
+        print("Please choose between rock, paper or scissors")
 
