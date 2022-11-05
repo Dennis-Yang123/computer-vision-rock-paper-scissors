0a68efa67e30dce62a339c1cc3fd5077e1db8c6b
Dennis Yang <dennisyang3.14@gmail.com>
2022-11-05 19:04:11 +0000

    Milestone 4 Task 2

--- manual_rps.py
+++ manual_rps.py
@@ -2,8 +2,8 @@
 import random
 rps = ["rock", "paper", "scissors"]
 def get_computer_choice():
-    comp_choice = random.choice(rps)
-    print(comp_choice)
+    computer_choice = random.choice(rps)
+    print(computer_choice)
 
 def get_user_choice():
     user_choice = input("Please choose between rock, paper or scissors")
@@ -12,4 +12,16 @@ def get_user_choice():
         print(user_choice)
     else:
         print("Please choose between rock, paper or scissors")
+
+def get_winner(computer_choice, user_choice):
+    if computer_choice == user_choice:
+        print("It's a tie!")
+    elif computer_choice == "rock" and user_choice == "scissors":
+        print("You lost")
+    elif computer_choice == "paper" and user_choice == "rock":
+        print("You lost")
+    elif computer_choice == "scissors" and user_choice == "paper":
+        print("You lost")
+    else:
+        print("You won!")
 
