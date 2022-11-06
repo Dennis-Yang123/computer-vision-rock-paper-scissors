e798c3bec52cd99618da9289955696a3cb3ee503
Dennis Yang <dennisyang3.14@gmail.com>
2022-11-06 18:23:58 +0000

    Works with user camera input

--- camera_rps.py
+++ camera_rps.py
@@ -2,49 +2,50 @@
 import random
 from rock_paper_scissors import prediction
 import numpy as np
-rps = ["rock", "paper", "scissors"]
+rps = [0, 1, 2] # 0 = rock, 1 = paper, 2 = scissors
 def get_computer_choice():
     computer_choice = random.choice(rps)
     print(computer_choice)
     return computer_choice
 
-def get_user_choice():
-    user_choice = input("Please choose between rock, paper or scissors")
-    user_choice = user_choice.lower()
-    if user_choice in rps:
-        print(user_choice)
-    else:
-        print("Please choose between rock, paper or scissors")
-    return user_choice
+# def get_user_choice():
+    # user_choice = input("Please choose between rock, paper or scissors")
+    # user_choice = user_choice.lower()
+    # if user_choice in rps:
+        #print(user_choice)
+    # else:
+        #print("Please choose between rock, paper or scissors")
+    # return user_choice
     
 
 def get_winner(computer_choice, user_choice):
     if computer_choice == user_choice:
         print("It's a tie!")
-    elif computer_choice == "rock" and user_choice == "scissors":
+    elif computer_choice == 0 and user_choice == 2: # rock vs scissors
         print("You lost")
-    elif computer_choice == "paper" and user_choice == "rock":
+    elif computer_choice == 1 and user_choice == 0: # paper vs rock
         print("You lost")
-    elif computer_choice == "scissors" and user_choice == "paper":
+    elif computer_choice == 2 and user_choice == 1: # scissors vs paper
         print("You lost")
     else:
         print("You won!")
     
 def get_prediction():
     print(prediction)
-    probability = np.argmax(prediction)
-    if probability == 0:
+    user_choice = np.argmax(prediction)
+    if user_choice == 0:
         print("You chose rock")
-    elif probability == 1:
+    elif user_choice == 1:
         print("You've chosen paper")
-    elif probability == 2:
+    elif user_choice == 2:
         print("You've chosen scissors")
     else:
         print("You chose nothing you are nothing")
+    return user_choice
 
 def play():
-    get_winner(get_computer_choice(), get_user_choice())
-    get_prediction()
+    get_winner(get_computer_choice(), get_prediction())
+
 play()
 
 # %%
